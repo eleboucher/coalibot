fcf323a27370f0784027da53bce402ea217a8d62
Erwan Leboucher <erwanleboucher@gmail.com>
2018-02-23 18:27:37 +0100

    src/slack_rtm.js -> src/index.js

--- src/42_api.js
+++ src/42_api.js
@@ -6,7 +6,7 @@
 /*   By: elebouch <elebouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 21:07:36 by elebouch          #+#    #+#             */
-/*   Updated: 2018/02/23 13:27:56 by elebouch         ###   ########.fr       */
+/*   Updated: 2018/02/23 18:17:57 by elebouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ const {
     postOnThread,
     getUsername,
     postAttachments
-} = require('./slack_api');
-const rq = require('./request').rq;
-const ClientOAuth2 = require('client-oauth2');
-const { month } = require('./const');
-const moment = require('moment');
-const sprintf = require('sprintf-js').sprintf;
+} = require("./slack_api");
+const rq = require("./request").rq;
+const ClientOAuth2 = require("client-oauth2");
+const { month } = require("./const");
+const moment = require("moment");
+const sprintf = require("sprintf-js").sprintf;
 
 const forty2auth = new ClientOAuth2({
     clientId: process.env.INTRA_CLIENT_ID,
     clientSecret: process.env.INTRA_SECRET,
-    accessTokenUri: 'https://api.intra.42.fr/oauth/token'
+    accessTokenUri: "https://api.intra.42.fr/oauth/token"
 });
 
 const request42 = async url => {
-    var url = 'https://api.intra.42.fr' + url;
+    var url = "https://api.intra.42.fr" + url;
     const token = await forty2auth.credentials.getToken();
     var options = {
         uri: url,
@@ -40,7 +40,7 @@ const request42 = async url => {
             access_token: token.data.access_token
         },
         headers: {
-            'User-Agent': 'Request-Promise'
+            "User-Agent": "Request-Promise"
         },
         json: true // Automatically parses the JSON string in the response
     };
@@ -52,66 +52,66 @@ const request42 = async url => {
 };
 
 const alliance = async channel => {
-    const json = await request42('/v2/coalitions');
+    const json = await request42("/v2/coalitions");
     json.sort(function(a, b) {
         return a.score < b.score;
     });
     let rang = 0;
-    while (json[rang]['id'] !== 2) rang += 1;
+    while (json[rang]["id"] !== 2) rang += 1;
     if (rang === 0) {
-        const diff = json[rang]['score'] - json[1]['score'];
+        const diff = json[rang]["score"] - json[1]["score"];
         postMessage(`Felicitations Nous sommes premiers avec ${rang + 1} points d'avance. :the-alliance:`, channel);
     } else {
-        const diff = json[0]['score'] - json[rang]['score'];
+        const diff = json[0]["score"] - json[rang]["score"];
         postMessage(`Nous sommes à la ${rang + 1}eme place avec ${diff} points de retard. :the-alliance:`, channel);
     }
 };
 
 const score = async channel => {
-    const json = await request42('/v2/coalitions');
+    const json = await request42("/v2/coalitions");
     json.sort(function(a, b) {
         return a.score < b.score;
     });
-    var reply = '';
+    var reply = "";
     for (let coa of json) {
-        reply += '${name} ${score}\n';
+        reply += "${name} ${score}\n";
     }
     var attachments = [
         {
             fallback: reply,
-            color: json[0]['color'],
-            author_link: 'https://profile.intra.42.fr/blocs/1/coalitions',
+            color: json[0]["color"],
+            author_link: "https://profile.intra.42.fr/blocs/1/coalitions",
             fields: [
                 {
-                    title: json[0]['name'],
-                    value: json[0]['score'],
+                    title: json[0]["name"],
+                    value: json[0]["score"],
                     short: true
                 },
                 {
-                    title: json[1]['name'],
-                    value: String(json[1]['score'] + ' (' + Number(json[1]['score'] - json[0]['score']) + ')'),
+                    title: json[1]["name"],
+                    value: String(json[1]["score"] + " (" + Number(json[1]["score"] - json[0]["score"]) + ")"),
                     short: true
                 },
                 {
-                    title: json[2]['name'],
-                    value: String(json[2]['score'] + ' (' + Number(json[2]['score'] - json[0]['score']) + ')'),
+                    title: json[2]["name"],
+                    value: String(json[2]["score"] + " (" + Number(json[2]["score"] - json[0]["score"]) + ")"),
                     short: true
                 },
                 {
-                    title: json[3]['name'],
-                    value: String(json[3]['score'] + ' (' + Number(json[3]['score'] - json[0]['score']) + ')'),
+                    title: json[3]["name"],
+                    value: String(json[3]["score"] + " (" + Number(json[3]["score"] - json[0]["score"]) + ")"),
                     short: true
                 }
             ],
-            footer: 'Powered by Coalibot'
+            footer: "Powered by Coalibot"
         }
     ];
-    postAttachments('', attachments, channel);
+    postAttachments("", attachments, channel);
 };
 
 const get_range_logtime = async (user, range_begin, range_end) => {
-    range_begin = moment(range_begin).format('YYYY-MM-DD');
-    range_end = moment(range_end).format('YYYY-MM-DD');
+    range_begin = moment(range_begin).format("YYYY-MM-DD");
+    range_end = moment(range_end).format("YYYY-MM-DD");
     range_date = `?page[size]=100&range[begin_at]=${range_begin},${range_end}`;
     url = `/v2/users/${user}/locations/${range_date}`;
     const data = await request42(url);
@@ -124,9 +124,9 @@ const get_range_logtime = async (user, range_begin, range_end) => {
             let i = 2;
             let ret = data;
             do {
-                last_location = moment(ret[ret.length - 1]['begin_at']);
+                last_location = moment(ret[ret.length - 1]["begin_at"]);
                 if (moment(range_begin).isBefore(last_location)) {
-                    tmp = await request42(url + '&page[number]=' + i);
+                    tmp = await request42(url + "&page[number]=" + i);
                     if (tmp) {
                         ret = ret.concat(tmp);
                     }
@@ -140,9 +140,9 @@ const get_range_logtime = async (user, range_begin, range_end) => {
         let locations = await get_more(data);
         let logtime = moment.duration(0);
         for (let x of locations) {
-            if (x['end_at']) log_end = moment(x['end_at']);
+            if (x["end_at"]) log_end = moment(x["end_at"]);
             else log_end = moment();
-            log_start = moment(x['begin_at']);
+            log_start = moment(x["begin_at"]);
             log_session = log_end - log_start;
             logtime.add(log_session);
         }
@@ -153,56 +153,56 @@ const get_range_logtime = async (user, range_begin, range_end) => {
 };
 
 const format_output_datetime = time => {
-    const timem = Number(time.as('minutes'));
+    const timem = Number(time.as("minutes"));
     const hours = Math.floor(timem / 60);
     const min = Math.floor(timem % 60);
     return [hours, min];
 };
 
 const profil = async (user, channel) => {
-    url = '/v2/users/' + user;
-    urlcoal = url + '/coalitions/';
+    url = "/v2/users/" + user;
+    urlcoal = url + "/coalitions/";
     const data = await request42(url);
     let lvl = 1;
     const coaldata = await request42(urlcoal);
     if (!data) {
-        postMessage('invalid login', channel);
+        postMessage("invalid login", channel);
         return;
     }
     let lvlpiscine = 0;
-    if (data['pool_year'] === '2013' || data['pool_year'] === '2014') {
+    if (data["pool_year"] === "2013" || data["pool_year"] === "2014") {
         lvlpiscine = 0;
-    } else if (data['cursus_users'].length === 1) {
-        lvlpiscine = data['cursus_users'][0]['level'];
+    } else if (data["cursus_users"].length === 1) {
+        lvlpiscine = data["cursus_users"][0]["level"];
         lvl = 0;
-    } else lvlpiscine = data['cursus_users'][1]['level'];
-    let coalslug = '';
-    if (coaldata.length) coalslug = ':' + coaldata[0]['slug'] + ':';
+    } else lvlpiscine = data["cursus_users"][1]["level"];
+    let coalslug = "";
+    if (coaldata.length) coalslug = ":" + coaldata[0]["slug"] + ":";
     range_end = moment();
-    range_begin = moment().subtract(7, 'days');
+    range_begin = moment().subtract(7, "days");
     const logtime = await get_range_logtime(user, range_begin, range_end);
-    var time = format_output_datetime(logtime);
-    graph = 'https://projects.intra.42.fr/projects/graph?login=' + user;
+    const time = format_output_datetime(logtime);
+    graph = "https://projects.intra.42.fr/projects/graph?login=" + user;
     const stage = (data => {
         const ret = {
-            finished: 'A fait son',
-            in_progress: 'En cours de'
+            finished: "A fait son",
+            in_progress: "En cours de"
         };
         const u = data.projects_users.find(d => d.project.id === 118);
-        return u ? ret[u['status']] : "N'a pas fait son";
+        return u ? ret[u["status"]] : "N'a pas fait son";
     })(data);
     postMessage(
         sprintf(
-            '%s %s\nPhoto: `%s`\nTemps de log cette semaine %02d:%02d\nNiveau: %.2f\nNiveau piscine  %.2f %s %s\n%s stage\nGraph: %s',
-            data['displayname'],
+            "%s %s\nPhoto: `%s`\nTemps de log cette semaine %02d:%02d\nNiveau: %.2f\nNiveau piscine  %.2f %s %s\n%s stage\nGraph: %s",
+            data["displayname"],
             coalslug,
-            data['image_url'],
+            data["image_url"],
             time[0],
             time[1],
-            lvl === 0 ? 0 : data['cursus_users'][0]['level'],
+            lvl === 0 ? 0 : data["cursus_users"][0]["level"],
             lvlpiscine,
-            data['pool_month'],
-            data['pool_year'],
+            data["pool_month"],
+            data["pool_year"],
             stage,
             graph
         ),
@@ -211,67 +211,67 @@ const profil = async (user, channel) => {
 };
 
 const logtime = async (message, channel, ts) => {
-    if (message.split(' ').length < 4) {
+    if (message.split(" ").length < 4) {
         postOnThread('Usage: bc logtime login datedebut datefin (date au format "Y-M-D")', channel, ts);
     } else if (
-        message.split(' ').length === 4 &&
-        !isNaN(message.split(' ')[3]) &&
-        parseInt(message.split(' ')[3]) > 2012
+        message.split(" ").length === 4 &&
+        !isNaN(message.split(" ")[3]) &&
+        parseInt(message.split(" ")[3]) > 2012
     ) {
         let date_begin = moment({
-            y: parseInt(message.split(' ')[3]),
+            y: parseInt(message.split(" ")[3]),
             M: 0,
             d: 1
         });
         let date_end = moment({
-            y: parseInt(message.split(' ')[3]),
+            y: parseInt(message.split(" ")[3]),
             M: 11,
             d: 31
         });
-        const logtime = await get_range_logtime(message.split(' ')[2], date_begin, date_end);
-        var time = format_output_datetime(logtime);
+        const logtime = await get_range_logtime(message.split(" ")[2], date_begin, date_end);
+        const time = format_output_datetime(logtime);
         postOnThread(sprintf(`%02dh%02d`, time[0], time[1]), channel, ts);
     } else if (
-        message.split(' ')[3].includes('trimestre') &&
-        (message.split(' ').length === 4 || (message.split(' ').length === 5 && parseInt(message.split(' ')[4]) > 2012))
+        message.split(" ")[3].includes("trimestre") &&
+        (message.split(" ").length === 4 || (message.split(" ").length === 5 && parseInt(message.split(" ")[4]) > 2012))
     ) {
-        let quarter = parseInt(message.split(' ')[3].replace('trimestre', '')) - 1;
+        let quarter = parseInt(message.split(" ")[3].replace("trimestre", "")) - 1;
         let year;
-        if (message.split(' ').length === 5 && parseInt(message.split(' ')[4]) > 2012)
-            year = parseInt(message.split(' ')[4]);
+        if (message.split(" ").length === 5 && parseInt(message.split(" ")[4]) > 2012)
+            year = parseInt(message.split(" ")[4]);
         else year = new Date().getFullYear();
         let date_begin = moment(new Date(year, quarter * 3, 1));
-        let date_end = moment(new Date(year, date_begin.get('month') + 3, 0));
-        const logtime = await get_range_logtime(message.split(' ')[2], date_begin, date_end);
-        var time = format_output_datetime(logtime);
+        let date_end = moment(new Date(year, date_begin.get("month") + 3, 0));
+        const logtime = await get_range_logtime(message.split(" ")[2], date_begin, date_end);
+        const time = format_output_datetime(logtime);
         postOnThread(sprintf(`%02dh%02d`, time[0], time[1]), channel, ts);
     } else if (
-        message.split(' ')[3] in month &&
-        (message.split(' ').length === 4 || (message.split(' ').length === 5 && parseInt(message.split(' ')[4]) > 2012))
+        message.split(" ")[3] in month &&
+        (message.split(" ").length === 4 || (message.split(" ").length === 5 && parseInt(message.split(" ")[4]) > 2012))
     ) {
-        if (message.split(' ').length === 5 && parseInt(message.split(' ')[4]) > 2012)
-            year = parseInt(message.split(' ')[4]);
+        if (message.split(" ").length === 5 && parseInt(message.split(" ")[4]) > 2012)
+            year = parseInt(message.split(" ")[4]);
         else year = new Date().getFullYear();
-        let date_begin = moment(new Date(year, month[message.split(' ')[3]], 1));
-        let date_end = moment(new Date(year, month[message.split(' ')[3]] + 1, 0));
-        const logtime = await get_range_logtime(message.split(' ')[2], date_begin, date_end);
+        let date_begin = moment(new Date(year, month[message.split(" ")[3]], 1));
+        let date_end = moment(new Date(year, month[message.split(" ")[3]] + 1, 0));
+        const logtime = await get_range_logtime(message.split(" ")[2], date_begin, date_end);
         var time = format_output_datetime(logtime);
         postOnThread(sprintf(`%02dh%02d`, time[0], time[1]), channel, ts);
-    } else if (message.split(' ').length === 5) {
+    } else if (message.split(" ").length === 5) {
         let date_end;
-        if (message.split(' ')[4] === 'today') date_end = moment();
-        else date_end = moment(message.split(' ')[4]);
-        let date_begin = moment(message.split(' ')[3]);
+        if (message.split(" ")[4] === "today") date_end = moment();
+        else date_end = moment(message.split(" ")[4]);
+        let date_begin = moment(message.split(" ")[3]);
         if (date_end.isValid() && date_begin.isValid()) {
-            const logtime = await get_range_logtime(message.split(' ')[2], date_begin, date_end);
-            var time = format_output_datetime(logtime);
+            const logtime = await get_range_logtime(message.split(" ")[2], date_begin, date_end);
+            const time = format_output_datetime(logtime);
             postOnThread(sprintf(`%02dh%02d`, time[0], time[1]), channel, ts);
         }
     }
 };
 
 const who = async (place, channel) => {
-    if (!place || place.startsWith('!') || place.startsWith('?')) return;
+    if (!place || place.startsWith("!") || place.startsWith("?")) return;
     const url = `/v2/campus/1/locations/?filter[host]=${place}&filter[active]=true`;
     const data = await request42(url);
     if (!data) {
@@ -279,24 +279,24 @@ const who = async (place, channel) => {
         return;
     }
     if (data.length === 0) postMessage(`Place *${place}* vide`, channel);
-    else postMessage(`*${data[0]['user']['login']}* est à la place *${place}*`, channel);
+    else postMessage(`*${data[0]["user"]["login"]}* est à la place *${place}*`, channel);
 };
 
 const where = async (user, channel) => {
-    if (!user || user.startsWith('!') || user.startsWith('?')) return;
-    if (user === 'queen' || user == 'way') {
+    if (!user || user.startsWith("!") || user.startsWith("?")) return;
+    if (user === "queen" || user == "way") {
         postMessage("follow me bruddah\ni'll show you de way :uganda_knuckles:", channel);
         return;
     }
-    if (user === 'dieu') user = 'elebouch';
+    if (user === "dieu") user = "elebouch";
     url = `/v2/users/${user}/locations`;
     const data = await request42(url);
     if (!data) {
         postMessage(`login invalide`, channel);
         return;
     }
-    if (data.length === 0 || data[0]['end_at']) postMessage(`*${user}* est hors ligne`, channel);
-    else postMessage(`*${user}* est à la place *${data[0]['host']}*`, channel);
+    if (data.length === 0 || data[0]["end_at"]) postMessage(`*${user}* est hors ligne`, channel);
+    else postMessage(`*${user}* est à la place *${data[0]["host"]}*`, channel);
 };
 
 module.exports.alliance = alliance;
