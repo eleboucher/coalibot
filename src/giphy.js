181c2dc21c9b2817a1acfca17309b97e200b8ec3
Erwan Leboucher <erwanleboucher@gmail.com>
2018-02-25 12:11:08 +0100

    catch if request return 404

--- src/giphy.js
+++ src/giphy.js
@@ -6,7 +6,7 @@
 /*   By: elebouch <elebouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/24 15:03:45 by elebouch          #+#    #+#             */
-/*   Updated: 2018/02/24 17:43:31 by elebouch         ###   ########.fr       */
+/*   Updated: 2018/02/25 12:10:29 by elebouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@ const { postMessage, postUserMessage, sendReaction, fileUpload, postOnThread, ge
 
 const randomgif = async (query, channel) => {
     //not my api key :3:
-    const res = await rp(
-        `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=aH0B2QT0mjnLZ7xKvHTwURhIHcIiB4MR&limit=30`,
-        { json: true }
-    );
-    const theOnlyOne = res.data[Math.floor(Math.random() * Math.floor(res.data.length - 1))].url;
-    postMessage(theOnlyOne, channel);
+    try {
+        const res = await rp(
+            `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=aH0B2QT0mjnLZ7xKvHTwURhIHcIiB4MR&limit=30`,
+            { json: true }
+        );
+        const theOnlyOne = res.data[Math.floor(Math.random() * Math.floor(res.data.length - 1))].url;
+        postMessage(theOnlyOne, channel);
+    } catch (err) {
+        postMessage("Aucun gif trouvé", channel);
+    }
 };
 
 module.exports.randomgif = randomgif;
