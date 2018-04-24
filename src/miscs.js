1f63b5509f2861bff962e8a3d88324f8495b979c
genesixx <erwanleboucher@gmail.com>
2018-04-24 18:33:57 +0200

    format output

--- src/miscs.js
+++ src/miscs.js
@@ -6,7 +6,7 @@
 /*   By: elebouch <elebouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 14:27:58 by elebouch          #+#    #+#             */
-/*   Updated: 2018/04/24 18:33:11 by elebouch         ###   ########.fr       */
+/*   Updated: 2018/04/24 18:33:47 by elebouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,10 +130,10 @@ const roulette = async (channel, user) => {
     username = ''
   }
   if (russiantab[0] === 1) {
-    postMessage(`<@${username}>: Bang (${6 - russiantab.length + 1} / 6 )`, channel)
+    postMessage(`<@${username}>: Bang ( ${6 - russiantab.length + 1} / 6 )`, channel)
     russiantab = []
   } else {
-    postMessage(`<@${username}>: click (${6 - russiantab.length + 1} / 6 )`, channel)
+    postMessage(`<@${username}>: click ( ${6 - russiantab.length + 1} / 6 )`, channel)
     russiantab.shift()
   }
 }
