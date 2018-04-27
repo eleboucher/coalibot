d67a0e2dfbed6b8baa6c809fd2b0f3cc365d3a65
Erwan Leboucher <erwanleboucher@gmail.com>
2018-04-27 20:33:10 +0200

    don't quote

--- src/miscs.js
+++ src/miscs.js
@@ -203,7 +203,7 @@ const roulettetop = async (channel)  => {
   sorted = await Object.keys(json).sort(function(a, b) { return json[a] - json[b] }).reverse().reduce((r, k) => (r[k] = json[k], r), {});
   i = 1
   for (let o in sorted){
-      await postMessage(`${i} <@${o}> : ${sorted[o]}`, channel)
+      await postMessage(`${i} ${o} : ${sorted[o]}`, channel)
       if (i === 5) break
       i++
   }
