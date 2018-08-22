8ef1dff672ed5587d2eb0b6daad6ef0718c7bca1
Erwan Leboucher <erwanleboucher@gmail.con>
2018-08-22 15:46:48 +0200

    watch files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
       env: {
         NODE_ENV: 'development'
       },
+      watch: true,
+      ignore_watch: '*.json',
       env_production: {
         NODE_ENV: 'production'
       }
