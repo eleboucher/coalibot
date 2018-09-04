08d99f16a874adfa9cf7af567af1f9537e799b9c
Erwan LEBOUCHER <elebouch@e2r10p3.42.fr>
2018-09-04 17:39:20 +0200

    fix month

--- src/logtime.js
+++ src/logtime.js
@@ -6,7 +6,7 @@
 /*   By: elebouch <elebouch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 23:24:22 by elebouch          #+#    #+#             */
-/*   Updated: 2018/09/04 00:06:52 by elebouch         ###   ########.fr       */
+/*   Updated: 2018/09/04 17:39:06 by elebouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -170,10 +170,7 @@ const handleDate = (message, option) => {
 const handleMonth = (message, option) => {
   option.count += 1
   if (
-    message
-      .split(' ')
-      [option.count].normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '') in month
+    message.split(' ')[option.count].normalize('NFD').replace(/[\u0300-\u036f]/g, '') in month
   ) {
     let year = ''
     if (
@@ -181,7 +178,7 @@ const handleMonth = (message, option) => {
       parseInt(message.split(' ')[option.count + 1]) > 2000
     ) {
       year = parseInt(message.split(' ')[option.count + 1])
-      option.count += 1
+      option.month += 1
     } else year = new Date().getFullYear()
     option.date_begin = moment({
       y: year,
@@ -190,7 +187,7 @@ const handleMonth = (message, option) => {
     })
     option.date_end = moment({
       y: year,
-      M: month[message.split(' ')[option.count - 1]],
+      M: month[message.split(' ')[option.count]],
       d: 31
     }).endOf('day')
   } else if (
