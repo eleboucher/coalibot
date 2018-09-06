21c9c0b96eed0a27feef9d46900a845ec5fca43f
Erwan Leboucher <erwanleboucher@gmail.com>
2018-09-06 21:52:27 +0200

    wip

--- command.go
+++ command.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
+
+	"github.com/nlopes/slack"
 )
 
 func indexOf(word string, data []string) int {
@@ -14,25 +19,59 @@ func indexOf(word string, data []string) int {
 	return -1
 }
 
-func reply()
+func reply(command string, event *Message) bool {
+	// Open our jsonFile
+	jsonFile, err := os.Open("reply.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	// a map container to decode the JSON structure into
+	c := make(map[string]interface{})
 
-// m := map[string] fn {
-//     "f": foo,
-//     "b": bar,
-//   }
+	// unmarschal JSON
+	e := json.Unmarshal(byteValue, &c)
+	if e != nil || c[command] == nil {
+		return false
+	}
 
-func handleCommand(message *Message) {
+	// output result to STDOUT
+	fmt.Printf("reply %s\n", c[command].(string))
+	event.API.PostMessage(event.Channel, c[command].(string), slack.PostMessageParameters{})
+	return true
+}
+
+func hello(option string, event *Message) bool {
+	event.API.PostMessage(event.Channel, "Hello <@"+event.User+"> ! powered by go", slack.PostMessageParameters{})
+	return true
+}
+
+var commands = map[string]func(string, *Message) bool{
+	"hello": hello,
+}
+
+func handleCommand(event *Message) {
 	var isCommand = false
 	var option = ""
 	var command = ""
-	// remove extra whitespace
-	message.Message = strings.Join(strings.Fields(message.Message), " ")
 
-	fmt.Printf("<#%s> @%s: %s\n", message.Channel, message.User, message.Message)
-	splited := strings.Split(message.Message, " ")
+	event.Message = strings.Join(strings.Fields(event.Message), " ")
+	fmt.Printf("<#%s> @%s: %s\n", event.Channel, event.User, event.Message)
+	splited := strings.Split(event.Message, " ")
 	if indexOf(splited[0], []string{"coalibot", "bc", "cb"}) > -1 && len(splited) > 1 {
-		fmt.Printf("Coalibot command\n")
+		command = splited[1]
+		option = strings.Join(splited[1:], " ")
+		isCommand = reply(command, event)
+		if !isCommand {
+			if isCommand, err := commands[command](option, event); ok {
+
+			}
+		}
 	} else if splited[0][0] == '!' && len(splited[0]) > 1 {
+
 		fmt.Printf("Coalibot command\n")
 	}
 
