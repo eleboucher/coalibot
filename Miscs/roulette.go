0fb982c8f61c43bcb56db9a7ec3a9324b841a996
Erwan Leboucher <erwanleboucher@gmail.com>
2018-09-07 13:53:01 +0200

    use default params

--- Miscs/roulette.go
+++ Miscs/roulette.go
@@ -16,16 +16,16 @@ func Roulette(option string, event *Struct.Message) bool {
 			tab = append(tab, 0)
 		}
 		tab[rand.Intn(6)] = 1
-		event.API.PostMessage(event.Channel, "On recharge le revolver!", slack.PostMessageParameters{})
+		event.API.PostMessage(event.Channel, "On recharge le revolver!", slack.NewPostMessageParameters())
 	}
 	var count = 6 - len(tab) + 1
 	if tab[0] == 1 {
 		tab = nil
-		event.API.PostMessage(event.Channel, "<@"+event.User+">: Bang ( "+strconv.Itoa(count)+" / 6 )", slack.PostMessageParameters{})
+		event.API.PostMessage(event.Channel, "<@"+event.User+">: Bang ( "+strconv.Itoa(count)+" / 6 )", slack.NewPostMessageParameters())
 		// handlestat(user)
 	} else {
 		tab = tab[1:]
-		event.API.PostMessage(event.Channel, "<@"+event.User+">: Click ( "+strconv.Itoa(count)+" / 6 )", slack.PostMessageParameters{})
+		event.API.PostMessage(event.Channel, "<@"+event.User+">: Click ( "+strconv.Itoa(count)+" / 6 )", slack.NewPostMessageParameters())
 	}
 	return true
 }
