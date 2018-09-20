78ccb5cde3c690bc6472ea94a8415e2ea982291a
Erwan Leboucher <erwanleboucher@gmail.com>
2018-09-20 10:34:59 +0200

    one roulette per chan

--- Miscs/roulette.go
+++ Miscs/roulette.go
@@ -8,23 +8,23 @@ import (
 	"github.com/genesixx/coalibot/Utils"
 )
 
-var tab []int
+var tab = make(map[string][]int)
 
 func Roulette(option string, event *Struct.Message) bool {
-	if tab == nil || len(tab) == 0 {
+	if tab[event.Channel] == nil || len(tab[event.Channel]) == 0 {
 		for i := 0; i < 6; i++ {
-			tab = append(tab, 0)
+			tab[event.Channel] = append(tab[event.Channel], 0)
 		}
-		tab[rand.Intn(6)] = 1
+		tab[event.Channel][rand.Intn(6)] = 1
 		event.API.PostMessage(event.Channel, "On recharge le revolver!", Struct.SlackParams)
 	}
-	var count = 6 - len(tab) + 1
-	if tab[0] == 1 {
+	var count = 6 - len(tab[event.Channel]) + 1
+	if tab[event.Channel][0] == 1 {
 		tab = nil
 		event.API.PostMessage(event.Channel, "<@"+event.User+">: Bang ( "+strconv.Itoa(count)+" / 6 )", Struct.SlackParams)
 		Utils.HandleRouletteStat(event)
 	} else {
-		tab = tab[1:]
+		tab[event.Channel] = tab[event.Channel][1:]
 		event.API.PostMessage(event.Channel, "<@"+event.User+">: Click ( "+strconv.Itoa(count)+" / 6 )", Struct.SlackParams)
 	}
 	return true
