636716093260101d2267e6a7d06eada388df1fba
Charles-Antoine Alba <35659800+Porow@users.noreply.github.com>
2018-10-23 14:56:52 +0200

    removed fciprian

--- Users/anroche.go
+++ Users/anroche.go
@@ -28,20 +28,3 @@ func Anroche(option string, event *Struct.Message) bool {
 	event.API.PostMessage(event.Channel, fmt.Sprintf("Fin du stage dans %02d jours, %02d heures %02d minutes %02d secondes :e2r5:", d, h, m, s), Struct.SlackParams)
 	return true
 }
-
-func Fciprian(option string, event *Struct.Message) bool {
-	loc, _ := time.LoadLocation("Europe/Paris")
-	stage, _ := time.ParseInLocation("2006-01-02 15:04", "2018-11-27 18:30", loc)
-	ts := -time.Since(stage)
-
-	ts += +Decisecond / 2
-	d := (ts / Day)
-	ts = ts % Day
-	h := ts / time.Hour
-	ts = ts % time.Hour
-	m := ts / time.Minute
-	ts = ts % time.Minute
-	s := ts / time.Second
-	event.API.PostMessage(event.Channel, fmt.Sprintf("Délivrance dans %02d jours, %02d heures %02d minutes %02d secondes, d, h, m, s), Struct.SlackParams)
-	return true
-}							 
