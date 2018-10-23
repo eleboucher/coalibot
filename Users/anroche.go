8df2d9135b5f88e2e6557583efd45642e16c5914
Charles-Antoine Alba <35659800+Porow@users.noreply.github.com>
2018-10-23 14:27:28 +0200

    removed calba to separate it

--- Users/anroche.go
+++ Users/anroche.go
@@ -29,23 +29,6 @@ func Anroche(option string, event *Struct.Message) bool {
 	return true
 }
 
-func Calba(option string, event *Struct.Message) bool {
-	loc, _ := time.LoadLocation("Europe/Paris")
-	stage, _ := time.ParseInLocation("2006-01-02 15:04", "2018-12-03 17:15", loc)
-	ts := -time.Since(stage)
-
-	ts += +Decisecond / 2
-	d := (ts / Day)
-	ts = ts % Day
-	h := ts / time.Hour
-	ts = ts % time.Hour
-	m := ts / time.Minute
-	ts = ts % time.Minute
-	s := ts / time.Second
-	event.API.PostMessage(event.Channel, fmt.Sprintf("Libération dans %02d jours, %02d heures %02d minutes %02d secondes, d, h, m, s), Struct.SlackParams)
-	return true
-}
-
 func Fciprian(option string, event *Struct.Message) bool {
 	loc, _ := time.LoadLocation("Europe/Paris")
 	stage, _ := time.ParseInLocation("2006-01-02 15:04", "2018-11-27 18:30", loc)
