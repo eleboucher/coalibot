53d9de2fbe4a797651fd6b2271dd86c556dd8876
Erwan Leboucher <erwanleboucher@Erwans-MacBook-Pro.local>
2018-10-25 18:12:13 +0200

    fix coin

--- Miscs/coin.go
+++ Miscs/coin.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Coin(option string, event *Struct.Message) bool {
-	if rand.Intn(1) == 0 {
+	if rand.Intn(2) == 0 {
 		event.API.PostMessage(event.Channel, "<@"+event.User+">: Heads", Struct.SlackParams)
 	} else {
 		event.API.PostMessage(event.Channel, "<@"+event.User+">: Tails", Struct.SlackParams)
