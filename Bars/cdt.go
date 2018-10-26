c27745021db6b12aeb5ad887c2514287e4be224e
Erwan Leboucher <erwanleboucher@Erwans-MacBook-Pro.local>
2018-10-26 12:07:42 +0200

    add CDT

--- Bars/cdt.go
+++ Bars/cdt.go
@@ -1,13 +1,66 @@
 package Bars
 
-import "github.com/genesixx/coalibot/Struct"
+import (
+	"time"
+
+	"github.com/genesixx/coalibot/Struct"
+	"github.com/nlopes/slack"
+)
 
 func Cdt(option string, event *Struct.Message) bool {
-	if IsSpritzOpen() {
-		event.API.PostMessage(event.Channel, "Go Spritz!", Struct.SlackParams)
-		return true
-	} else {
-		event.API.PostMessage(event.Channel, "Le Spritz reste mieux!", Struct.SlackParams)
+	open := "Fermé !"
+	color := "danger"
+	if IsCdtOpen() {
+		open = "Ouvert !"
+		color = "good"
+	}
+	params := Struct.SlackParams
+	attachment := slack.Attachment{
+		Color: color,
+		Title: "Café du Théatre",
+		Text:  open,
+		Fields: []slack.AttachmentField{
+			slack.AttachmentField{
+				Title: "Horaire",
+				Value: "8-00h tous les jours!",
+			},
+			slack.AttachmentField{
+				Title: "Mell's Brau :beer:",
+				Value: "Pinte: 4.50€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Pelforth :beer:",
+				Value: "Pinte: 5.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "La Chouffe :beer: :gnome:",
+				Value: "Pinte: 6.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Cuvée des trolls/Vedett/Affligem :beer:",
+				Value: "Pinte: 6.50€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Vin :wine_glass:",
+				Value: "4.80€",
+				Short: true,
+			},
+		},
+		Footer: "Powered by Coalibot",
+	}
+	params.Attachments = []slack.Attachment{attachment}
+	event.API.PostMessage(event.Channel, "", params)
+	return true
+}
+
+func IsCdtOpen() bool {
+	now := time.Now()
+	if now.Hour() >= 8 && now.Hour() < 24 {
 		return true
 	}
+	return false
 }
