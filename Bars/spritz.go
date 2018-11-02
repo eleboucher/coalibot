9a4d21f6bd205306189b0eb1793c163a77d5f0f3
Erwan Leboucher <erwanleboucher@gmail.com>
2018-11-02 10:29:23 +0100

    add spritz event

--- Bars/spritz.go
+++ Bars/spritz.go
@@ -1,6 +1,7 @@
 package Bars
 
 import (
+	"strings"
 	"time"
 
 	"github.com/genesixx/coalibot/Struct"
@@ -10,6 +11,10 @@ import (
 
 func Spritz(option string, event *Struct.Message) bool {
 
+	if strings.ToLower(strings.Split(option, " ")[0]) == "event" {
+		eventBDE(event)
+		return true
+	}
 	open := "Fermé !"
 	color := "danger"
 	if IsSpritzOpen() {
@@ -90,4 +95,61 @@ func IsSpritzOpen() bool {
 		return true
 	}
 	return false
+}
+
+func eventBDE(event *Struct.Message) {
+	open := "Fermé !"
+	color := "danger"
+	now := time.Now()
+	date, _ := time.Parse("2006-01-02", "2018-11-02")
+
+	if now.Day() == date.Day() && now.Month() == date.Month() && now.Year() == date.Year() && now.Hour() >= 18 && now.Hour() < 21 || (now.Hour() == 21 && now.Minute() <= 30) {
+		open = "Ouvert !"
+		color = "good"
+	}
+	params := Struct.SlackParams
+	attachment := slack.Attachment{
+		Color: color,
+		Title: "Spritz",
+		Text:  open,
+		Fields: []slack.AttachmentField{
+			slack.AttachmentField{
+				Title: "Event Blood Horror Party",
+				Value: "Prix reduit sous presentation de billet de la soirée",
+			},
+			slack.AttachmentField{
+				Title: "Ricard :sunny:",
+				Value: "2.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Kronenbourg :beer:",
+				Value: "Demi: 2.50€ Pinte: 4.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Gallia :beer: :cucco:",
+				Value: "Demi: 3.00€ Pinte: 5.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Liqueur/Shot :tumbler_glass:",
+				Value: "2.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Cocktail Special Halloween" + Utils.Choice([]string{":cocktail2:", ":cocktail:"}),
+				Value: "5.00€",
+				Short: true,
+			},
+			slack.AttachmentField{
+				Title: "Whisky Coca / Pomme :whiskey2:",
+				Value: "5.00€",
+				Short: true,
+			},
+		},
+		Footer: "Powered by Coalibot",
+	}
+	params.Attachments = []slack.Attachment{attachment}
+	event.API.PostMessage(event.Channel, "", params)
 }
