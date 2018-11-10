0afaa4119ebb9609c2942d818ecce933e5bd86ff
Erwan Leboucher <erwanleboucher@gmail.com>
2018-11-10 15:02:46 +0100

    add english month name

--- FortyTwo/logtime.go
+++ FortyTwo/logtime.go
@@ -61,6 +61,18 @@ var monthL = map[string]int{
 	"octobre":   10,
 	"novembre":  11,
 	"decembre":  12,
+	"january":   1,
+	"february":  2,
+	"march":     3,
+	"april":     4,
+	"may":       5,
+	"june":      6,
+	"july":      7,
+	"august":    8,
+	"september": 9,
+	"october":   10,
+	"november":  11,
+	"december":  12,
 }
 
 var zeroDate = time.Time{}
