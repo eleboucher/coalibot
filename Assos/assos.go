81cee5af67ff8c4b51f6103a1c994aca7e5dfd49
Erwan Leboucher <erwanleboucher@gmail.com>
2018-11-13 08:11:19 +0100

    fix link for real

--- Assos/assos.go
+++ Assos/assos.go
@@ -6,17 +6,17 @@ import (
 )
 
 func Assos(option string, event *Struct.Message) bool {
-	var assos = `<#bde>
-<#42genesys> :42genesys:
-<#42entrepreneurs> :42entrepreneurs:
-<#42_ai> :42ai:
-<#42born2music> :musical_note:
-<#association-sans-nom> :asn-party2:
-<#esport42> :esport42:
-<#escape-games>
-<#airsoft42>  :ak-42:
-<#code_her>  :codeher:
-<#42zen>`
+	var assos = `<#C04GT8U3Y|bde>
+<#C8C0EGZUY|42genesys> :42genesys:
+<#C03BN553Z|42entrepreneurs> :42entrepreneurs:
+<#C4LF6DS82|42_ai> :42ai:
+<#C03EZET39|42born2music> :musical_note:
+<#C4VMBA7P1|association-sans-nom> :asn-party2:
+<#C0HTYJR99|esport42> :esport42:
+<#C736Z2BNZ|escape-games>
+<#C7X7V8HV5|airsoft42>  :ak-42:
+<#C4W29G7N2|code_her>  :codeher:
+<#C8U8Z88TB|42zen>`
 	event.API.PostMessage(event.Channel, slack.MsgOptionText(assos, false))
 	return true
 }
