0254d0a7d10d46e9cf2381ae33a3750e42928d05
Erwan Leboucher <erwanleboucher@gmail.com>
2018-11-23 09:23:32 +0100

    Fix the haters

--- Users/elebouch.go
+++ Users/elebouch.go
@@ -11,7 +11,7 @@ import (
 func Elebouch(option string, event *Struct.Message) bool {
 	loc, _ := time.LoadLocation("Europe/Paris")
 	stage, _ := time.ParseInLocation("2006-01-02 15:04", "2018-11-05 09:30", loc)
-	ts := -time.Since(stage)
+	ts := time.Since(stage)
 
 	ts += +Decisecond / 2
 	d := (ts / Day)
@@ -21,6 +21,6 @@ func Elebouch(option string, event *Struct.Message) bool {
 	m := ts / time.Minute
 	ts = ts % time.Minute
 	s := ts / time.Second
-	event.API.PostMessage(event.Channel, slack.MsgOptionText(fmt.Sprintf("Debut du stage dans %02d jours, %02d heures %02d minutes %02d secondes", d, h, m, s), false))
+	event.API.PostMessage(event.Channel, slack.MsgOptionText(fmt.Sprintf("En stage depuis %02d jours, %02d heures %02d minutes %02d secondes", d, h, m, s), false))
 	return true
 }
