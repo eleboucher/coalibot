034e5a47ed4b311da275592862a6c2ad4aa69f65
Erwan Leboucher <erwanleboucher@gmail.com>
2019-02-07 22:05:24 +0100

    fix path

--- Twitch/emotes.go
+++ Twitch/emotes.go
@@ -8,7 +8,7 @@ import (
 
 func Emotes(option string, event *Struct.Message) bool {
 	params := slack.FileUploadParameters{
-		File:     "emotes/" + option + ".png",
+		File:     "Twitch/emotes/" + option + ".png",
 		Channels: []string{event.Channel},
 	}
 	event.API.UploadFile(params)
