3335c6aa9e48e015f36152ae0dacf83610acddfc
Erwan Leboucher <erwanleboucher@gmail.com>
2019-02-09 00:55:19 +0100

    add filename

--- Twitch/emotes.go
+++ Twitch/emotes.go
@@ -10,6 +10,7 @@ func Emotes(option string, event *Struct.Message) bool {
 	params := slack.FileUploadParameters{
 		File:     "Twitch/emotes/" + option + ".png",
 		Channels: []string{event.Channel},
+		Filename: option + ".png",
 	}
 	event.API.UploadFile(params)
 	return true
