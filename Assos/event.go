8bf355b52656c4917ec7e8e5a945d461155a0e4e
Erwan Leboucher <erwanleboucher@gmail.com>
2019-05-16 20:39:53 +0200

    Change Event

--- Assos/event.go
+++ Assos/event.go
@@ -11,11 +11,11 @@ func Event(option string, event *Struct.Message) bool {
 	params.IconURL = "https://bde.student42.fr/img/bde42-logo-1538664197.jpg"
 	params.Username = "Undefined Bot"
 	attachment := slack.Attachment{
-		Title:     "Quarante d'Œuf Easter Party",
-		TitleLink: "https://www.facebook.com/events/1011459799052089/",
+		Title:     "Barbec' Petanque Cup Pong",
+		TitleLink: "https://www.facebook.com/events/398544317410148",
 		Text:      "Le BDE undefined 42, en partenariat avec CPP et 42Green vous invitent ce jeudi 23 mai 2019 à venir partager avec nous une journée récréative, qui se terminera avec un barbecue offert à tous les étudiants (vegan compatible) !",
 		Footer:    "Powered by Coalibot",
-		Color:     "#009a49",
+		Color:     "#87CEEB",
 		Fields: []slack.AttachmentField{
 			slack.AttachmentField{
 				Title: "Date",
@@ -30,17 +30,17 @@ func Event(option string, event *Struct.Message) bool {
 			slack.AttachmentField{
 				Title: "Dés 10h, et jusque 14h environ, tournoi de pétanque.",
 				Value: "Inscriptions ci-dessous : https://doodle.com/poll/eda7pqvb2khyk8se Puis pétanque libre à partir de 14h, jusqu'à la tombée de la nuit !",
-				Short: true,
+				Short: false,
 			},
 			slack.AttachmentField{
 				Title: "Dés 14h, et jusque 23h, tournoi de cup pong.",
 				Value: "Inscriptions ci-dessous : 14h-18h : https://doodle.com/poll/puftq63gkbsu6a8r 18h-22h : https://doodle.com/poll/97ynm78u4zv27f8w",
-				Short: true,
+				Short: false,
 			},
 			slack.AttachmentField{
 				Title: "Dés 19h, barbecue gratuit, sans inscriptions.",
 				Value: "",
-				Short: true,
+				Short: false,
 			},
 			slack.AttachmentField{
 				Title: "Buvette",
