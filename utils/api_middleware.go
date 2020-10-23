3614488c0df50efaf537e4bb958fc85a81126c48
Erwan Leboucher <erwanleboucher@gmail.com>
2020-10-23 13:35:43 +0200

    responde the error in a thread for the api

--- utils/api_middleware.go
+++ utils/api_middleware.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/slack-go/slack"
 
 	"gitlab.com/clafoutis/api42"
 )
@@ -16,6 +17,11 @@ func FortyTwoMiddleware(fn func(string, *Message) bool) func(string, *Message) b
 			client, err := api42.NewAPI(os.Getenv("INTRA_CLIENT_ID"), os.Getenv("INTRA_SECRET"))
 			if err != nil {
 				log.Errorf("Error with the api, %s", err)
+				PostMsg(
+					config,
+					slack.MsgOptionText(err.Error(), false),
+					slack.MsgOptionTS(config.Timestamp),
+				)
 			}
 			config.FortyTwo = client
 		}
